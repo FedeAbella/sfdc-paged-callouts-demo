136b9f53c74c06f4e8c75e273a1a2f23bfe4b5fd
Federico Abella <fede689@gmail.com>
2022-06-10 23:09:05 -0300

    added partial data and empty paged data methods to data-api

--- data-api/data-api.py
+++ data-api/data-api.py
@@ -1,3 +1,4 @@
+from crypt import methods
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
@@ -5,17 +6,26 @@ import pandas as pd
 app = Flask(__name__)
 api = Api(app)
 
-df = pd.read_csv('data-api/data_complete.csv')
-full_data = df.to_dict('records')
-full_response= {
-    'data':full_data
+df = pd.read_csv('data_complete.csv')
+complete_data = df.to_dict('records')
+complete_response = {
+    'data':complete_data
 }
 
-class Data(Resource):
-    def get(self):
-        return full_response, 200
+@app.route('/complete', methods=['GET'])
+def get_complete_data():
+    return complete_response, 200
 
-api.add_resource(Data, '/data')
+@app.route('/partial', method=['GET'])
+def get_partial_data():
+    partial_data = df[:1000].to_dict('records')
+    return {
+        'data':partial_data
+    }, 200
+
+@app.route('/paged', methods=['GET'])
+def get_paged_data():
+    pass
 
 if __name__ == '__main__':
     app.run()
