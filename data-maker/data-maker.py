834df06fc19efc3e897750af2f514ac9e8c4a4ce
Federico Abella <fede689@gmail.com>
2022-06-14 21:52:21 -0300

    increased dataset size, added Batch Apex, developer log object and process for sending emails when logs are created

--- data-maker/data-maker.py
+++ data-maker/data-maker.py
@@ -7,7 +7,7 @@ from faker import Faker
 import random
 import csv
 
-num_records = 250_000 # number of records to create
+num_records = 500_000 # number of records to create
 columns = ['id', 'name', 'job', 'company'] # column names
 
 fake = Faker() # instatiate Faker
