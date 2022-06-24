0cb8afd7ea058c7fb5fd6f2754624810af29bbb3
Federico Abella <fede689@gmail.com>
2022-06-24 16:51:09 -0300

    Removed random endpoint, coalesced complete and partial into one

--- data-api/data-api.py
+++ data-api/data-api.py
@@ -4,20 +4,17 @@ This is a simple Flask api that returns some data saved into a
     'data_complete.csv' file in the root script directory.
 
 Endpoints:
-    /complete: Returns a 200 code, and a JSON with the key 'data' containing
-        the full dataset
-    /partial: Returns a 200 code, and a partial set of data, formatted as with
-        /complete, and taken from the top. Takes the URL parameter 'size', with
-        values 'small', 'medium', 'large' (default 'small'), and returns the 
-        first 0.1%, 1%, or 10% records, rounded down. The 'data' key will be 
+    /data: Returns a 200 code, and a part of the dataset, taken from the top. 
+        Takes the URL parameter 'size', with values 'small', 'medium', 'large'
+        or 'complete' (default 'complete'), and returns the first 0.1%, 1%, 
+        10%, or 100% of records, rounded down. The 'data' key will be 
         empty if the parameter value is not enough to return at least 1 record.
-    /random: Works as /partial, but the data is taken at random from the entire
-        dataset. Rows are randomized as well.
     /paged: Takes in two URL parameters ('start' and 'end'), and returns the
         rows of data in between (first row is #1). If 'start' is larger than 
         the dataset size, the 'data' key in response is empty. If 'start' is
         within the dataset but 'end' is larger than the size, returns as much
-        as it can.
+        as it can. The 'size' parameter determines the maximum number of rows
+        the method will return (which is the last page)
     /: Just a simple html index page, to avoid 404.
 """
 from flask import Flask, request, jsonify
@@ -33,41 +30,32 @@ TOTAL_ROWS = DATASET.shape[0]
 PARTIAL_SIZES = {
     'small': 0.001,
     'medium': 0.01,
-    'large': 0.1
+    'large': 0.1,
+    'complete': 1
 }
 
-FAILURE_PROB = 0.2 # Probabiliy that a call to the 'faulty' endpoint fails
+FAILURE_PROB = 0.3 # Probabiliy that a call to the 'faulty' endpoint fails
 seed(a=None) # Initialize random to current time
 
-@app.route('/complete', methods=['GET'])
-def get_complete_data():
+@app.route('/data', methods=['GET'])
+def get_data():
     """
-    Returns the complete dataset as a JSON with the 'data' key containing it
-    """
-    return jsonify(
-        {
-            'success': True,
-            'data': DATASET.to_dict('records')
-        }
-    ), 200
-
-@app.route('/partial', methods=['GET'])
-def get_partial_data():
-    """
-    Returns a partial set of data, taken from the top. Takes the URL parameter
-    'size' with values 'small', 'medium', 'large' (default 'small') and returns
-    the top 0.1%, 1% or 10% of data respectively. Returns 400 and error message
-    if wrong parameter value is passed.
+    Returns a part of the dataset, taken from the top. Takes the URL parameter
+    'size' with values 'small', 'medium', 'large', or 'complete' (default
+    'complete') and returns the top 0.1%, 1%, 10% or 100% of data respectively.
+    Returns 400 and error message if wrong parameter value is passed. The data
+    is returned in JSON form with the 'data' key containing a list of objects,
+    while the 'success' key returns a boolean.
     """
     response = {}
 
-    # check for the right parameter value, or none passed (default to small)
-    size = request.args.get('size', 'small')
+    # check for the right parameter value, or none passed (default to complete)
+    size = request.args.get('size', 'complete')
     if size not in PARTIAL_SIZES.keys():
         # if passed the wrong parameter, return 400 and error message
         response['success'] = False
         response['error'] = "'size' parameter takes only values 'small',"\
-            " 'medium' or 'large'."
+            " 'medium', 'large' or 'complete'."
         return jsonify(response), 400
 
     # calculate number of rows to return, and default 'data' key to empty
@@ -80,57 +68,34 @@ def get_partial_data():
 
     return jsonify(response), 200
 
-@app.route('/random', methods=['GET'])
-def get_random_data():
+@app.route('/paged', methods=['GET'])
+def get_paged_data():
     """
-    Returns a random subset of data. Works the same as /partial, but instead
-    of taking the top portion of the data, it samples the appropriate amount of
-    rows and shuffles them before returning.
+    Returns a paged portion of the data. Works the same as the 'data' endpoint, 
+    but also takes in two required, positive integer parameters: 'start' and 
+    'end', with 'end' >= 'start'. Returns all rows between 'start' and 'end' 
+    (included) as a list in the 'data' key. First row is numbered 1. 
+    If 'start' is larger than the dataset size, returns an empty list in the 
+    key. If 'end' is larger than the dataset size, returns as much as possible. 
+    If any parameter is missing or not valid, returns 400 and an error message.
     """
+
     response = {}
 
-    # check for the right parameter value, or none passed (default to small)
-    size = request.args.get('size', 'small')
+    # get the 'size' parameter, default as 'complete' if missing
+    size = request.args.get('size', 'complete')
     if size not in PARTIAL_SIZES.keys():
         # if passed the wrong parameter, return 400 and error message
         response['success'] = False
         response['error'] = "'size' parameter takes only values 'small',"\
-            " 'medium' or 'large'."
+            " 'medium', 'large' or 'complete'."
         return jsonify(response), 400
 
-    # calculate number of rows to return, and default 'data' key to empty
-    rows_to_return = floor( TOTAL_ROWS * PARTIAL_SIZES.get(size) )
-    response['success'] = True
-    response['data'] = []
-    # if there's anything to return, get a random sample of data and use that
-    if rows_to_return > 0:
-        response['data'] = DATASET.sample(rows_to_return).to_dict('records')
-
-    return jsonify(response), 200
-
-
-@app.route('/paged', methods=['GET'])
-def get_paged_data():
-    """
-    Returns a paged portion of the data. Takes in two required, positive
-    integer parameters: 'start' and 'end', with 'end' >= 'start'. Returns all
-    rows between 'start' and 'end' (included) as a list in the 'data' key. 
-    First row is numbered 1. If 'start' is larger than the dataset size, 
-    returns an empty list in the key. If 'end' is larger than the dataset size,
-    returns as much as possible. If any parameter is missing or not valid, 
-    returns 400 and an error message.
-    """
+    max_rows = floor( TOTAL_ROWS * PARTIAL_SIZES.get(size) )
 
     # get the 'start' and 'end' parameters
     start = request.args.get('start', None)
     end = request.args.get('end', None)
-    is_quick_demo = request.args.get('quick', None)
-
-    max_rows = TOTAL_ROWS
-    if is_quick_demo:
-        max_rows = floor( TOTAL_ROWS * PARTIAL_SIZES.get('large') )
-
-    response = {}
 
     # check both parameters are present
     if not start or not end:
@@ -186,7 +151,7 @@ def get_faulty_data():
 
     # There's a probability that the endpoint simply returns a server error
     if (random() < FAILURE_PROB):
-        return 500
+        return 'Internal Server Error', 500
 
     return get_paged_data()
 
